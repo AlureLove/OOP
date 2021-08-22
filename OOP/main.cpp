0c956175eec4492782d2bc162986f6d25eddd748
AluerLove <qhlrx@qq.com>
2021-08-22 21:15:30 +0800

    test commit

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -2,14 +2,31 @@
 //  main.cpp
 //  OOP
 //
-//  Created by 刘睿轩 on 2021/8/22.
+//  Created by guchen on 2021/8/22.
 //
 
 #include <iostream>
+using namespace std;
+
+const double PI = 3.14;
+
+class Circle {
+
+public:
+    int m_r;
+    double calculateZC() {
+        return 2 * PI * m_r;
+    }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+
+    Circle c1;
+
+    c1.m_r = 10;
+
+    cout << c1.calculateZC() << endl;
 
     system("pause");
     return 0;
